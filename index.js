0e353d3c366ef75b902dd64e9dfbc6d9c08240ab
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2022-06-08 23:37:18 +0430

    push code for debug

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 require("dotenv").config();
-const axios = require("axios");
 const runJsonServer = require("./data/runJsonServer");
+const crud = require("./routes/crud");
 
 const app = express();
 runJsonServer();
 
-app.get("/", async (req, res) => {
-  const data = await axios.get("http://localhost:8080/data");
-  res.send(data.data);
+app.get("/", (req, res) => {
+  res.send("hello friends");
 });
+
+app.use("/data", crud);
 
 const PORT = 5000;
 app.listen(PORT, () => {
